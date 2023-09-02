6d4f353d0e7b5bbe629382ab80ae70f1f46ae24d
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-02 12:59:28 -0400

    style: styling components for different breakpoints

--- src/components/PetFinder/SearchBar.vue
+++ src/components/PetFinder/SearchBar.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="w-full h-[14rem] bg-orange-500 relative md:h-[7rem]">
     <div
-      class="absolute w-[90%] h-[90%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] md:w-full"
+      class="absolute w-[90%] h-[90%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] md:w-3/4 md:h-[35%] md:top-20 md:p-1 md:bg-white border border-black"
     >
-      <div class="w-full h-full flex flex-wrap items-center">
+      <div class="w-full h-full flex flex-wrap items-center md:flex-row">
         <input
-          class="w-full h-1/4 px-3 rounded placeholder:uppercase"
+          class="w-full h-1/4 px-3 rounded placeholder:uppercase md:w-[55%] md:h-full"
           placeholder="Enter your location zip code"
           v-model="location"
           aria-required="true"
@@ -14,7 +14,7 @@
         <select
           aria-required="true"
           v-model="selected"
-          class="w-full h-1/4 px-3 rounded"
+          class="w-full h-1/4 px-3 rounded bg-gray-200 md:w-[30%] md:h-full md:rounded-none"
         >
           <option disabled value="">Please select one</option>
           <option>Cat</option>
@@ -23,11 +23,11 @@
           <option>Bird</option>
         </select>
 
-        <div class="w-full h-1/4 flex justify-evenly py-1">
+        <button class="md:w-[15%] md:h-full bg-orange-500">Search</button>
+        <!-- <div class="w-full h-1/4 flex justify-evenly py-1">
           <SmallButton label="Clear" @handleEvent="clear" />
           <SmallButton label="Search" @handleEvent="submit" />
-        </div>
-        <div>{{ location }}</div>
+        </div> -->
       </div>
     </div>
   </div>
