c64c4709d2dbe6017c35d34b7236ea99a201eaba
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-02 12:38:27 -0400

    feat: created SmallButton component

---
+++ src/components/Buttons/SmallButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    class="w-2/5 bg-orange-500 rounded uppercase font-semibold tracking-wide border border-black active:bg-orange-300"
+    @click="btnClicked"
+  >
+    {{ props.label }}
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  label: { type: String, required: true },
+});
+
+const emit = defineEmits(["handleEvent"]);
+
+function btnClicked() {
+  emit("handleEvent");
+}
+</script>
