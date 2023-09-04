3fe83c215387d9eee02993512ac6ff96a8479041
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-04 10:47:10 -0400

    feat: created Regular Button component

---
+++ src/components/Buttons/RegularButton.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    @click="btnClicked"
+    class="hidden md:w-[15%] md:h-full md:bg-orange-500 md:flex md:items-center md:justify-between md:px-[6px] md:text-white xl:justify-evenly"
+  >
+    <Icon
+      icon="foundation:magnifying-glass"
+      color="white"
+      :horizontalFlip="true"
+    />
+    <span>{{ props.label }}</span>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  label: { type: String, required: true },
+});
+
+const emit = defineEmits(["handleEvent"]);
+
+function btnClicked() {
+  emit("handleEvent");
+}
+</script>
