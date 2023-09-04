d6358e9b86ceaa7d85e1333cd8331c7ca78c5d28
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-04 10:55:06 -0400

    refactor: used reusable buttons components

--- src/components/PetFinder/SearchBar.vue
+++ src/components/PetFinder/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="w-full h-[14rem] bg-orange-500 relative md:h-[7rem]">
     <div
-      class="absolute w-[90%] h-[90%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] md:w-3/4 md:h-[35%] md:top-20 md:p-1 md:bg-white border border-black"
+      class="absolute w-[90%] h-[90%] top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] rounded md:w-3/4 md:h-[35%] md:top-24 md:p-1 md:bg-white lg:w-[55%]"
     >
       <div class="w-full h-full flex flex-wrap items-center md:flex-row">
         <input
@@ -16,18 +16,19 @@
           v-model="selected"
           class="w-full h-1/4 px-3 rounded bg-gray-200 md:w-[30%] md:h-full md:rounded-none"
         >
-          <option disabled value="">Please select one</option>
+          <option disabled value="">Select Pet</option>
           <option>Cat</option>
           <option>Dog</option>
           <option>Rabbit</option>
           <option>Bird</option>
         </select>
 
-        <button class="md:w-[15%] md:h-full bg-orange-500">Search</button>
-        <!-- <div class="w-full h-1/4 flex justify-evenly py-1">
+        <div class="w-full h-1/4 flex justify-evenly py-1 md:hidden">
           <SmallButton label="Clear" @handleEvent="clear" />
           <SmallButton label="Search" @handleEvent="submit" />
-        </div> -->
+        </div>
+
+        <RegularButton label="Search" @handleEvent="submit" />
       </div>
     </div>
   </div>
@@ -36,6 +37,7 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import SmallButton from "../Buttons/SmallButton.vue";
+import RegularButton from "../Buttons/RegularButton.vue";
 
 const location = ref<number | undefined>(undefined);
 const selected = ref("");
