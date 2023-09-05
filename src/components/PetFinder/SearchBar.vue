36b5cb3ceec5ffebd6693d132cee887a2bccfd90
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-05 09:19:07 -0400

    feat: created getPets async function to fetch data from API and store it based on the information needed to be displayed

--- src/components/PetFinder/SearchBar.vue
+++ src/components/PetFinder/SearchBar.vue
@@ -25,10 +25,17 @@
 
         <div class="w-full h-1/4 flex justify-evenly py-1 md:hidden">
           <SmallButton label="Clear" @handleEvent="clear" />
-          <SmallButton label="Search" @handleEvent="submit" />
+          <SmallButton label="Search" @handleEvent="getPets" />
         </div>
 
-        <RegularButton label="Search" @handleEvent="submit" />
+        <RegularButton label="Search" @handleEvent="getPets" />
+      </div>
+      <div v-for="(pet, index) in store.pets" :key="index">
+        <img :src="pet.photo?.medium" :alt="pet.name" />
+        <span>Name: {{ pet.name }}</span>
+        <span>Age: {{ pet.age }}</span>
+        <span>Breed: {{ pet.breed }}</span>
+        <span>Gender: {{ pet.gender }}</span>
       </div>
     </div>
   </div>
@@ -36,13 +43,47 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { usePetDataStore } from "../../stores/PetDataStore";
 import SmallButton from "../Buttons/SmallButton.vue";
 import RegularButton from "../Buttons/RegularButton.vue";
 
 const location = ref<number | undefined>(undefined);
 const selected = ref("");
+const store = usePetDataStore();
+const token = ref(
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiJNb2VXdGIyQVhNa20wVXBCOUNacXNTTjRPWVZjOU9kdWxuREI5aDZBRjRqWnNlczhHZCIsImp0aSI6ImQ3OWE4ZTU2MDExNmFkOGUyYjEwMjc2Y2FhNWM5YWM5YTMwZjJmM2U2ZGVmMjVmMjZiYWY4YjhlMzI2YjhhZWUzNTU3ZmFiYzA1MzJhZGI5IiwiaWF0IjoxNjkzOTE5MjQ5LCJuYmYiOjE2OTM5MTkyNDksImV4cCI6MTY5MzkyMjg0OSwic3ViIjoiIiwic2NvcGVzIjpbXX0.DzaWVjG19a__J9vyVO2HKOyk_J2RPeVIy4BiYe_kXhMW6CKeRG-XgESuQn6r4WGNJjN5eKjuNavlHsVIeza9or3jH7JvCVFmUb0Q3CNeuUAkk5gi9ykAGmEorOL9EQ-Q9c2IdRonfqilciTR4A7ZsniIegZFsUBMyFH9VJ5yFTzi-R7VxENAZNXiuugXpCSnuINYASFihVNqSkhHg-ZyWtRF-JxmEHeKNj3BRiV55jXe5IafLL3IpnxB-65wQl-ycqieEaXJxBbi9vHj6G16SV041zvoHyV4f8GGFGEIRWmw7U39dgRvTxUOLllUg51gyzDuqQDy6wgASwgscYEXNw"
+);
+const BearerToken = `Bearer ${token.value}`;
 
-function submit() {}
+const options = {
+  method: "GET",
+  headers: {
+    Authorization: BearerToken,
+  },
+};
+
+async function getPets() {
+  try {
+    const res = await fetch(
+      "https://api.petfinder.com/v2/animals?type=dog&page=2",
+      options
+    );
+    const pets = await res.json();
+    let temp = pets.animals;
+    for (let i = 0; i < temp.length; i++) {
+      store.pets.push({
+        age: temp[i].age,
+        gender: temp[i].gender,
+        name: temp[i].name,
+        breed: temp[i].breeds.primary,
+        distance: temp[i].distance,
+        photo: temp[i].photos[0],
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
 
 function clear() {
   location.value = undefined;
