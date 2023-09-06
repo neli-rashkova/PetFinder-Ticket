9afc27bb13ddf9945b4a93ad26999171a5f19475
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-06 11:07:50 -0400

    feat: imported axios for vue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import "./assets/main.css";
 
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import axios from "axios";
+import VueAxios from "vue-axios";
 import App from "./App.vue";
 
 const pinia = createPinia();
 const app = createApp(App);
 
 app.use(pinia);
+app.use(VueAxios, axios);
 app.mount("#app");
