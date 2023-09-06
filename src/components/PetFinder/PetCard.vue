330fe4cc0b7d2b4a8905f6a54267daa52750aa65
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-06 19:15:46 -0400

    style: added condtional to show either img element or div with paw prints if no image is available

--- src/components/PetFinder/PetCard.vue
+++ src/components/PetFinder/PetCard.vue
@@ -1,5 +1,42 @@
 <template>
-  <div></div>
+  <div class="bg-white w-full h-[31.3rem] rounded">
+    <img
+      v-if="props.image?.length > 0"
+      class="w-full h-[75%] rounded-t"
+      :src="props.image"
+    />
+    <div
+      v-else
+      class="w-full h-[75%] rounded-t flex justify-center items-center"
+    >
+      <span class="text-[6rem] w-fit h-fit">&#128062;</span>
+    </div>
+    <section class="w-full h-1/4 px-4 pt-3 pb-2 flex flex-col justify-around">
+      <div class="flex flex-col">
+        <span class="text-lg font-semibold">{{ props.name }}</span>
+        <span class="text-gray-700 text-sm">{{ props.breed }}</span>
+      </div>
+      <div class="flex justify-between items-center">
+        <div
+          class="bg-orange-200 py-1 px-3 text-sm font-semibold text-orange-900 rounded-full"
+        >
+          {{ props.age }} | {{ props.gender }}
+        </div>
+        <div class="text-sm text-gray-600">
+          {{ Math.round(props.distance * 10) / 10 }} miles Away
+        </div>
+      </div>
+    </section>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const props = defineProps({
+  image: String,
+  name: String,
+  breed: String,
+  age: String,
+  gender: String,
+  distance: Number,
+});
+</script>
