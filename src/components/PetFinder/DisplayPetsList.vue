1d88590e2dcb064ec52957c4a6549a4beedb2db9
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-07 15:29:47 -0400

    style: added styles for different break points

--- src/components/PetFinder/DisplayPetsList.vue
+++ src/components/PetFinder/DisplayPetsList.vue
@@ -1,22 +1,22 @@
 <template>
   <div class="w-screen h-[calc(100vh-230px)] md:h-[calc(100vh-7rem)]">
     <div
-      class="w-full h-[93%] flex flex-col justify-between items-center pt-2 gap-1"
+      class="w-full h-[93%] flex flex-col justify-between items-center pt-2 gap-1 md:pt-4"
     >
       <div
         v-if="store.pets.length > 0"
         class="w-[90%] h-[10%] flex flex-col items-center"
       >
-        <h5 class="w-fit text-xl font-bold">
+        <h5 class="w-fit text-xl font-bold md:text-3xl">
           Search {{ inputStore.animalType }}
         </h5>
-        <h3 class="w-fit text-sm text-gray-700">
+        <h3 class="w-fit text-sm text-gray-700 md:text-base">
           Results for {{ inputStore.animalType.toLowerCase() }} near
           {{ inputStore.location }}
         </h3>
       </div>
       <div
-        class="w-[90%] h-[90%] overflow-x-scroll grid grid-cols-1 auto-rows-auto gap-y-5 md:grid-cols-2 md:gap-x-3 md:w-[97%]"
+        class="w-[90%] h-[90%] overflow-y-scroll grid grid-cols-1 auto-rows-auto gap-y-5 justify-items-center md:grid-cols-2 md:w-[95%] md:gap-y-4 lg:grid-cols-3 lg:gap-x-2 lg:gap-y-7 xl:grid-cols-4 xl:gap-x-0 xl:gap-y-14"
       >
         <PetCard
           v-for="pet in itemsOnCurrentPage"
