42a7bfaeb346a01869d4d33e2603b906299de963
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-07 10:27:23 -0400

    feat: imported DisplayPetsList component

--- src/components/PetFinder/index.vue
+++ src/components/PetFinder/index.vue
@@ -2,9 +2,11 @@
 <template>
   <div class="w-full h-full">
     <SearchBar />
+    <DisplayPetsList />
   </div>
 </template>
 
 <script setup lang="ts">
 import SearchBar from "./SearchBar.vue";
+import DisplayPetsList from "./DisplayPetsList.vue";
 </script>
