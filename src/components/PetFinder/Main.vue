ae68f77eb5507f89ba6bf885b835e5466c8ef188
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-08 16:00:33 -0400

    style: modified component name

---
+++ src/components/PetFinder/Main.vue
@@ -0,0 +1,12 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="w-full h-full">
+    <SearchBar />
+    <DisplayPetsList />
+  </div>
+</template>
+
+<script setup lang="ts">
+import SearchBar from "./SearchBar.vue";
+import DisplayPetsList from "./DisplayPetsList.vue";
+</script>
