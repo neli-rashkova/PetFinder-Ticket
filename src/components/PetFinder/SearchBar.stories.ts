52ee2090db1ec6d59d7219b14b446e464b815ef5
neli-rashkova <neli.rashkova73@gmail.com>
2023-09-08 16:01:40 -0400

    style: modified code

--- src/components/PetFinder/SearchBar.stories.ts
+++ src/components/PetFinder/SearchBar.stories.ts
@@ -1,43 +1,28 @@
-import type { Meta, StoryObj } from "@storybook/vue3";
-
 import SearchBar from "./SearchBar.vue";
 import RegularButton from "../Buttons/RegularButton.vue";
 import SmallButton from "../Buttons/SmallButton.vue";
 
-import { action } from "@storybook/addon-actions";
-
-const meta: Meta<typeof SearchBar> = {
+export default {
+  title: "SearchBar",
   component: SearchBar,
-  argTypes: {
-    onClick: { action: "clicked" },
+};
+
+export const WithRegularButton = () => ({
+  components: { SearchBar, RegularButton },
+  data() {
+    return {};
   },
-};
+  template: `
+  <SearchBar />
+  `,
+});
 
-export default meta;
-type Story = StoryObj<typeof SearchBar>;
-
-export const SearchBarWithSmallButton: Story = {
-  render: (args) => ({
-    components: { SearchBar, SmallButton },
-    setup() {
-      return {
-        ...args,
-        onClick: action("clicked"),
-      };
-    },
-    template: `<SearchBar @click="() =>({})" />`,
-  }),
-};
-
-export const SearchBarWithRegularButton: Story = {
-  render: (args) => ({
-    components: { SearchBar, RegularButton },
-    setup() {
-      return {
-        ...args,
-        onClick: action("clicked"),
-      };
-    },
-    template: `<SearchBar @click="() =>({})" />`,
-  }),
-};
+export const WithSmallButton = () => ({
+  components: { SearchBar, SmallButton },
+  data() {
+    return {};
+  },
+  template: `
+  <SearchBar />
+  `,
+});
